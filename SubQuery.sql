c02bd02c65dae93a82452f7981c5e2609d3dc2dd
asu kumar <asukumar.ug21.cs@nitp.ac.in>
2022-11-07 09:40:34 +0530

    first commit

---
+++ SubQuery.sql
@@ -0,0 +1,14 @@
+-- Active: 1664349590702@@127.0.0.1@5432
+CREATE DATABASE Techtfq;
+use Techtfq;
+	
+
+select * from employee;
+select * from department;
+select * from employee_history;
+select * from sales;
+
+-- Sub Query Tutorials
+
+
+
